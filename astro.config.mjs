d0233efab361a93ce10f938155205eacc93f2af8
epivitae <kwang@live.cn>
2026-05-12 17:25:52 +0800

    cf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    allowedHosts: true
+  },
+
+  // 你原来的 vite 配置不动
   vite: {
     plugins: [tailwindcss()]
   }
